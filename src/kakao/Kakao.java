4f48d442655ff148b9b776f5728831b42cb8dacb
Lusiotron2015 <Lusiotron2015@komp-w-salonie>
2015-06-02 23:33:50 +0200

    new commit

--- src/kakao/Kakao.java
+++ src/kakao/Kakao.java
@@ -15,11 +15,10 @@ public class Kakao {
      * @throws java.lang.Exception
      */
     public static void main(String[] args) throws Exception {
-        String sUrlString = "jdbc:sqlite:Cocoa.db";
-        KakaoDb kakaoDb = new KakaoDb("org.sqlite.JDBC",sUrlString);
+        //String sUrlString = "jdbc:sqlite:Cocoa.db";
+        //KakaoDb kakaoDb = new KakaoDb("org.sqlite.JDBC",sUrlString);
         ReadCSV obj = new ReadCSV();
 	obj.run();
-        
-    }
-    
+              
+    }       
 }
